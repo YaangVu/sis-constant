64009c189bd36fcd60e8c5b26d0981f045c062b9
keyhoang <hoangky@toprate.io>
2021-05-14 14:31:10 +0700

    update constant for user service

---
+++ Constant/RoleConstants.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Constants;
+
+
+class RoleConstants
+{
+    const ADMIN                = 'Admin';
+    const TEACHING_ASSISTANT   = 'Teaching assistant';
+    const TEACHER              = 'Teacher';
+    const HEADTEACHER          = 'Headteacher';
+    const PRINCIPAL            = 'Principal';
+    const ACADEMIC_COORDINATOR = 'Academic coordinator';
+    const SYSTEM               = 'System';
+    const STUDENT              = 'Student';
+    const FAMILY               = 'Family';
+}
---
+++ Constant/UserConstants.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace App\Constants;
+
+
+class UserConstants
+{
+    const SID = "sid";
+    const UID = "uid";
+}
