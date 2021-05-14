4af86fd65ff1fadec620c544c0b1ccbc5d2acc0b
keyhoang <hoangky@toprate.io>
2021-05-15 00:02:58 +0700

    comment constant

--- CodeConstant.php
+++ CodeConstant.php
@@ -6,6 +6,8 @@ namespace YaangVu\Constant;
 
 class CodeConstant
 {
-    const SID = "sid";
-    const UID = "uid";
+    const SC_ID = "sc_id"; // school code
+    const UID   = "uid"; // user code
+    const SID   = "sid"; // student code
+    const CID   = "cid"; // class code
 }
--- RegexConstant.php
+++ RegexConstant.php
@@ -6,5 +6,5 @@ namespace YaangVu\Constant;
 
 class RegexConstant
 {
-    const EMAIL = "^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$^";
+    const EMAIL = "^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$^"; // email validate
 }
