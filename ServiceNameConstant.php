f00872cb1847c832d6b5fb2bf853a5a3e2c3f73a
keyhoang <hoangky@toprate.io>
2021-05-15 22:20:14 +0700

    create service name constant

---
+++ ServiceNameConstant.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace YaangVu\Constant;
+
+
+class ServiceNameConstant
+{
+    const USER_SERVICE = "User-Service";
+    const SCHOOL_SERVICE = "School-Service";
+    const BASE_SERVICE = "Base-Service";
+}
