fd19f2b4143725ee49ee9c6b8bd7c44e7de4e96b
keyhoang <hoangky@toprate.io>
2021-05-21 22:02:58 +0700

    [UPDATE] RegexConstant.php

--- RegexConstant.php
+++ RegexConstant.php
@@ -9,5 +9,5 @@ class RegexConstant
     const EMAIL = "^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$^"; // email validate
     const PASSWORD = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,50}$/";
     const PHONE_NUMBER = "/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/";
-    const ONLY_NUMERIC = "/(01)[0-9]{9}";
+    const ONLY_NUMERIC = "/(01)[0-9]{9}/";
 }
