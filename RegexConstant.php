896cd8bec352d1ccbd94e8b60db92b0878319009
keyhoang <hoangky@toprate.io>
2021-05-26 23:59:32 +0700

    update regex phone_number + s3 constant

--- RegexConstant.php
+++ RegexConstant.php
@@ -8,6 +8,6 @@ class RegexConstant
 {
     const EMAIL = "^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$^"; // email validate
     const PASSWORD = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,50}$/";
-    const PHONE_NUMBER = "/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/";
+    const PHONE_NUMBER = "/^(0|\+84|84|\+1|1)((3[2-9])|(5[689])|(7[06-9])|(8[1-689])|(9[0-46-9]))(\d)(\d{3})(\d{3})$/";
     const ONLY_NUMERIC = "/(01)[0-9]{9}/";
 }
--- S3Constant.php
+++ S3Constant.php
@@ -7,5 +7,5 @@ namespace YaangVu\Constant;
 class S3Constant
 {
     const URL_S3 = "url_s3";
-    const ACTION_CREATE_USER = "create user";
+    const ACTION_CREATE_STAFF = "Import Staff";
 }
