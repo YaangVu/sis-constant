55ce252376f09538224a1a872ae080990f0ca978
keyhoang <hoangky@toprate.io>
2021-05-27 15:16:56 +0700

    [UPDATE] RegexConstant.php

--- RegexConstant.php
+++ RegexConstant.php
@@ -10,5 +10,5 @@ class RegexConstant
     const PASSWORD = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,50}$/";
     const PHONE_NUMBER = "/^(0|\+84|84|\+1|1)((3[2-9])|(5[689])|(7[06-9])|(8[1-689])|(9[0-46-9]))(\d)(\d{3})(\d{3})$/";
     const ONLY_NUMERIC = "/(01)[0-9]{9}/";
-    const USERNAME = "/^(?=.*[a-zA-Z]).{0,50}$/";
+    const USERNAME = "^[a-zA-Z0-9]{1,50}$";
 }
