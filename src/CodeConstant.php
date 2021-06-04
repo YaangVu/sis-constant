3222908c8f5e55a441d8207295a893d4cac18d7a
keyhoang <hoangky@toprate.io>
2021-06-04 14:21:27 +0700

    create lms system

--- src/CodeConstant.php
+++ src/CodeConstant.php
@@ -6,9 +6,10 @@ namespace YaangVu\Constant;
 
 class CodeConstant
 {
-    const SC_ID = "sc_id"; // school code
-    const UID   = "uid"; // user code
-    const SID   = "sid"; // student code
-    const CID   = "cid"; // class code
-    const EX_ID = 'external_id'; //external system ID
+    const SC_ID      = "sc_id"; // school code
+    const UID        = "uid"; // user code
+    const SID        = "sid"; // student code
+    const CID        = "cid"; // class code
+    const EX_ID      = 'external_id'; //external system ID
+    const LMS_SYSTEM = 'lms_system'; // LMS system
 }
---
+++ src/LmsSystemConstant.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace YaangVu\Constant;
+
+
+class LmsSystemConstant
+{
+    const EDMENTUM = "Edmentum";
+    const FLVS     = "FLVS";
+}
