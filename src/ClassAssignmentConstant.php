7631d6d00aac55f3e376ec2b9362ae2cbac5cc49
Yaang Vu <yaangvu@gmail.com>
2021-06-13 22:18:06 +0700

    add ClassAssignmentConstant

---
+++ src/ClassAssignmentConstant.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace YaangVu\Constant;
+
+
+class ClassAssignmentConstant
+{
+    const STUDENT            = 'Student';
+    const TEACHER            = 'Teacher';
+    const TEACHING_ASSISTANT = 'Teaching assistant';
+}
