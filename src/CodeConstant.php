cd442118bb670bfe30ca6e6099da750cdb099bc4
Yaang Vu <yaangvu@gmail.com>
2021-06-22 15:23:11 +0700

    add LID

--- src/CodeConstant.php
+++ src/CodeConstant.php
@@ -6,10 +6,12 @@ namespace YaangVu\Constant;
 
 class CodeConstant
 {
-    const SC_ID      = "sc_id"; // school code
-    const UID        = "uid"; // user code
-    const SID        = "sid"; // student code
-    const CID        = "cid"; // class code
-    const EX_ID      = 'external_id'; //external system ID
-    const LMS_SYSTEM = 'lms_system'; // LMS system
+    const SC_ID     = "sc_id"; // school code
+    const UID       = "uid"; // user code
+    const SID       = "sid"; // student code
+    const CID       = "cid"; // class code
+    const COURSE_ID = "co_id"; // course code
+    const PID       = "pr_id"; // program code
+    const EX_ID     = 'external_id'; //external system ID
+    const LID       = 'lid'; // LMS system ID
 }
