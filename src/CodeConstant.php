0da4d3726b61a29b2aeb53dddbd711ab1230f3d9
Yaang Vu <yaangvu@gmail.com>
2021-06-23 16:33:31 +0700

    Add term id

--- src/CodeConstant.php
+++ src/CodeConstant.php
@@ -6,12 +6,13 @@ namespace YaangVu\Constant;
 
 class CodeConstant
 {
-    const SC_ID     = "sc_id"; // school code
-    const UID       = "uid"; // user code
-    const SID       = "sid"; // student code
-    const CID       = "cid"; // class code
-    const COURSE_ID = "co_id"; // course code
-    const PID       = "pr_id"; // program code
-    const EX_ID     = 'external_id'; //external system ID
-    const LID       = 'lid'; // LMS system ID
+    const SC_ID = "sc_id"; // school code
+    const UID   = "uid"; // user code
+    const SID   = "sid"; // student code
+    const CID   = "cid"; // class code
+    const TID   = 'tid'; // term code
+    const CO_ID = "co_id"; // course code
+    const PID   = "pr_id"; // program code
+    const EX_ID = 'external_id'; //external system ID
+    const LID   = 'lid'; // LMS system ID
 }
