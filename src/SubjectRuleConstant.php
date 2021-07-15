750c2bd1c4a6caccb35e7a017989c62dc5055361
Yaang Vu <yaangvu@gmail.com>
2021-07-15 10:59:51 +0700

    add SubjectRuleConstant

---
+++ src/SubjectRuleConstant.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace YaangVu\Constant;
+
+
+class SubjectRuleConstant
+{
+    const   BEFORE            = 'before';
+    const   AFTER             = 'after';
+    const   PRECEDE           = 'precede';
+    const   FOLLOW            = 'follow';
+    const   CONSECUTIVE       = 'consecutive';
+    const   SAME_TEACHER      = 'same_teacher';
+    const   DIFFERENT_TEACHER = 'different_teacher';
+    const   SAME_PERIOD       = 'same_period';
+    const   DIFFERENT_PERIOD  = 'different_period';
+    const   SAME_TERM         = 'same_term';
+    const   DIFFERENT_TERM    = 'different_term';
+}
