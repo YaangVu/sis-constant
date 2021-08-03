aab6d47873671fc42e9fae3f1a697caa4b1523a1
Your NamePhuongNam160899 <you@example.comhaphuongnam81999@gmail.com>
2021-08-03 15:12:01 +0700

    add notification constant

--- src/NotificationConstant.php
+++ src/NotificationConstant.php
@@ -6,20 +6,20 @@ namespace YaangVu\Constant;
 
 class NotificationConstant
 {
-    const USERNAME     = '${username}';
-    const EMAIL        = '${email}';
-    const FULLNAME     = '${full_name}';
-    const STAFFID      = '${staffID}';
-    const STUDENTID    = '${studentID}';
-    const STUDENTGRADE = '${grade}';
+    const USERNAME      = '${username}';
+    const EMAIL         = '${email}';
+    const FULL_NAME     = '${full_name}';
+    const STAFF_ID      = '${staffID}';
+    const STUDENT_ID    = '${studentID}';
+    const STUDENT_GRADE = '${grade}';
 
     const ALL
         = [
             self::USERNAME,
             self::EMAIL,
-            self::FULLNAME,
-            self::STAFFID,
-            self::STUDENTID,
-            self::STUDENTGRADE,
+            self::FULL_NAME,
+            self::STAFF_ID,
+            self::STUDENT_ID,
+            self::STUDENT_GRADE,
         ];
 }
