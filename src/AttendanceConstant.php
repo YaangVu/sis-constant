aaee4c5ee2df2e0b9c84fbe1f341e911fa5102c3
Yaang Vu <yaangvu@gmail.com>
2021-08-12 16:45:09 +0700

    Add group for attendances

--- src/AttendanceConstant.php
+++ src/AttendanceConstant.php
@@ -13,9 +13,36 @@ class AttendanceConstant
     const ALL
         = [
             self::EX_ABSENCE,
+            self::UNEX_ABSENCE,
             self::PRESENT,
             self::EX_TARDY,
-            self::UNEX_ABSENCE,
             self::UNEX_TARDY
+        ];
+
+    ##################### GROUP #####################
+    const ATTEND  = 'attend';
+    const ABSENCE = 'absence';
+
+    const GROUP
+        = [
+            'attend'  => [
+                self::PRESENT,
+                self::EX_TARDY,
+                self::UNEX_TARDY
+            ],
+            'absence' => [
+                self::EX_ABSENCE,
+                self::UNEX_ABSENCE,
+            ]
+        ];
+
+    const GROUP_REVERSE
+        = [
+            self::EX_ABSENCE   => self::ABSENCE,
+            self::UNEX_ABSENCE => self::ABSENCE,
+
+            self::PRESENT    => self::ATTEND,
+            self::EX_TARDY   => self::ATTEND,
+            self::UNEX_TARDY => self::ATTEND
         ];
 }
