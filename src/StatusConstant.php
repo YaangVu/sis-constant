5035ee00ab907fb984e3e44eb7bd77124e86555f
keyhoang <hoangky@toprate.io>
2021-08-16 15:25:38 +0700

    update status constant

--- src/StatusConstant.php
+++ src/StatusConstant.php
@@ -6,22 +6,27 @@ namespace YaangVu\Constant;
 
 class StatusConstant
 {
-    const ACTIVE    = 'Active';
-    const INACTIVE  = 'Inactive';
-    const PENDING   = 'Pending';
-    const ARCHIVED  = 'Archived';
-    const DELETED   = 'Deleted';
-    const ON_GOING  = 'On-going';
-    const CONCLUDED = 'Concluded';
-
+    const ACTIVE     = 'Active';
+    const INACTIVE   = 'Inactive';
+    const PENDING    = 'Pending';
+    const ARCHIVED   = 'Archived';
+    const DELETED    = 'Deleted';
+    const ON_GOING   = 'On-going';
+    const CONCLUDED  = 'Concluded';
+    const STUDYING   = 'studying';
+    const WITHDRAWAL = 'withdrawal';
+    const GRADUATED  = 'graduated';
     const ALL
-        = [
+                     = [
             self::ACTIVE,
             self::INACTIVE,
             self::PENDING,
             self::ARCHIVED,
             self::DELETED,
             self::ON_GOING,
-            self::CONCLUDED
+            self::CONCLUDED,
+            self::STUDYING,
+            self::WITHDRAWAL,
+            self::GRADUATED,
         ];
 }
