0b6284936f211211083e30f468902207d5fbbc96
Nguyen Ngoc Chien <chienkakashi@gmail.com>
2021-08-17 11:21:37 +0700

    add constant subject

---
+++ src/Subject Constant.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace YaangVu\Constant;
+
+class SubjectConstant
+{
+    const   COMMON            = 'common';
+    const   HONORS            = 'honors';
+    const   ADVANCEDPLACEMENT = 'advanced placement';
+}
