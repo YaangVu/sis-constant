1c59b89f8e10146f4174d5984486a79fe61ccd8a
dung doan <doanvietdung0103@gmail.com>
2022-03-24 16:33:59 +0700

    create constant

---
+++ src/ToeflConstant.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @Author Dung
+ * @Date   Mar 24, 2022
+ */
+
+namespace YaangVu\Constant;
+
+class ToeflConstant
+{
+    const   LISTENING = 'listening';
+    const   WRITING   = 'writing';
+    const   SPEAKING  = 'speaking';
+    const   READING   = 'reading';
+
+}
