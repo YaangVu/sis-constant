308af64c03e81e76cddd230ef4ad588718678f8c
dung doan <doanvietdung0103@gmail.com>
2022-05-19 09:48:25 +0700

    update statusContanst

--- src/StatusConstant.php
+++ src/StatusConstant.php
@@ -6,28 +6,30 @@ namespace YaangVu\Constant;
 
 class StatusConstant
 {
-    const ACTIVE     = 'Active';
-    const INACTIVE   = 'Inactive';
-    const PENDING    = 'Pending';
-    const DONE       = 'Done';
-    const ARCHIVED   = 'Archived';
-    const DELETED    = 'Deleted';
-    const ON_GOING   = 'On-going';
-    const CONCLUDED  = 'Concluded';
-    const STUDYING   = 'studying';
-    const WITHDRAWAL = 'withdrawal';
-    const GRADUATED  = 'graduated';
-    const COMPLETED  = 'completed';
-    const GRADUATION = 'graduation';
-    const WITHDRAW   = 'withdraw';
-    const DROPOUT    = 'dropout';
-    const NOREVIEW   = 'No-review';
-    const REVIEW     = 'Review';
-    const APPROVE    = 'Approve';
-    const REJECT     = 'Reject';
-    const DRAFT      = 'Draft';
+    const ACTIVE      = 'Active';
+    const INACTIVE    = 'Inactive';
+    const PENDING     = 'Pending';
+    const DONE        = 'Done';
+    const ARCHIVED    = 'Archived';
+    const DELETED     = 'Deleted';
+    const ON_GOING    = 'On-going';
+    const CONCLUDED   = 'Concluded';
+    const STUDYING    = 'studying';
+    const WITHDRAWAL  = 'withdrawal';
+    const GRADUATED   = 'graduated';
+    const COMPLETED   = 'completed';
+    const GRADUATION  = 'graduation';
+    const WITHDRAW    = 'withdraw';
+    const DROPOUT     = 'dropout';
+    const NOREVIEW    = 'No-review';
+    const REVIEW      = 'Review';
+    const APPROVE     = 'Approve';
+    const REJECT      = 'Reject';
+    const DRAFT       = 'Draft';
+    const OFFICIAL    = 'Official';
+    const NOT_APPROVE = 'Not-approve';
     const ALL
-                     = [
+                      = [
             self::ACTIVE,
             self::INACTIVE,
             self::PENDING,
@@ -47,6 +49,8 @@ class StatusConstant
             self::REVIEW,
             self::APPROVE,
             self::REJECT,
-            self::DRAFT
+            self::DRAFT,
+            self::OFFICIAL,
+            self::NOT_APPROVE
         ];
 }
