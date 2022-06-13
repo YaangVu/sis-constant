20843770a52e1f1faddae363ba1a8ba0eae5a790
HoangKy <hoangky@toprate.io>
2022-06-13 10:36:23 +0700

    add job constant

---
+++ src/JobConstant.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @Author kyhoang
+ * @Date   Jun 10, 2022
+ */
+
+namespace YaangVu\Constant;
+
+class JobConstant
+{
+    const EMAIL        = 'email';
+    const NOTIFICATION = 'notification';
+    const SMS          = 'sms';
+}
