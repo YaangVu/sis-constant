c272b988490244f3d962c4a74caad611aaf5e03b
Hoang Viet <hoangviet10172001@gmail.com>
2022-06-13 09:53:21 +0700

    add type room meeting constan and status join rom meeting

---
+++ src/StatusJoinMeetingBeforeHost.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace YaangVu\Constant;
+
+
+class StatusJoinMeetingBeforeHost
+{
+    const ANYTIME = "anytime"; 
+    const FIVE_MINUTES = "5"; 
+    const TEN_MINUTES = "10"; 
+
+    const ALL
+        = [
+            self::ANYTIME, 
+            self::FIVE_MINUTES, 
+            self::TEN_MINUTES
+        ];
+}
