95f1dc91420e18823d754e8ea1e59f59d8db7694
Hoang Viet <hoangviet10172001@gmail.com>
2022-06-13 21:43:35 +0700

    add constant object guest invite to zoom

---
+++ src/GuestObjectInviteToZoomConstant.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @Author apple
+ * @Date   Jun 10, 2022
+ */
+
+namespace YaangVu\Constant;
+
+class GuestObjectInviteToZoomConstant
+{
+   const CLASSES = "class"; 
+   const USER_INFOMATION = "user-infomation"; 
+
+   const ALL = [
+      self::CLASSES, 
+      self::USER_INFOMATION
+   ]; 
+}
