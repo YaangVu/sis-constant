8624db21eeb92fe3b9b318c66ba248b16e96a9c5
HoangKy <hoangky@toprate.io>
2022-06-21 10:12:24 +0700

    create grade constant

---
+++ src/GradeConstant.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @Author kyhoang
+ * @Date   Jun 21, 2022
+ */
+
+namespace YaangVu\Constant;
+
+class GradeConstant
+{
+    const PS     = 'PS';
+    const PK     = 'PK';
+    const ONE    = '1';
+    const TWO    = '2';
+    const THREE  = '3';
+    const FOUR   = '4';
+    const FIVE   = '5';
+    const SIX    = '6';
+    const SEVEN  = '7';
+    const EIGHT  = '8';
+    const NINE   = '9';
+    const TEN    = '10';
+    const ELEVEN = '11';
+    const TWELVE = '12';
+
+    const GRADE_NOT_SYNC_STUDENT
+        = [
+            self::PK,
+            self::PS,
+        ];
+}
