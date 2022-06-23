1ef557c267e331d6379898c4b6644e2e9fdb65fb
Hoang Viet <hoangviet10172001@gmail.com>
2022-06-23 15:37:16 +0700

    update StatusJoinMeetingBeforeHostConstant

--- src/StatusJoinMeetingBeforeHostConstant.php
+++ src/StatusJoinMeetingBeforeHostConstant.php
@@ -6,14 +6,14 @@ namespace YaangVu\Constant;
 
 class StatusJoinMeetingBeforeHostConstant
 {
-    const ANYTIME = "0";
-    const FIVE_MINUTES = "5";
-    const TEN_MINUTES = "10"; 
+    const ANYTIME      = 0;
+    const FIVE_MINUTES = 5;
+    const TEN_MINUTES  = 10;
 
     const ALL
         = [
-            self::ANYTIME, 
-            self::FIVE_MINUTES, 
+            self::ANYTIME,
+            self::FIVE_MINUTES,
             self::TEN_MINUTES
         ];
 }
