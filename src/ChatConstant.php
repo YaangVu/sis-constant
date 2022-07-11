2d8b1d03d3f52262f70c0d551453004e6eeac92f
keyhoang <hoangky@toprate.io>
2022-07-11 22:12:45 +0700

    create chat constant

---
+++ src/ChatConstant.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @Author Edogawa Conan
+ * @Date   Jul 11, 2022
+ */
+
+namespace YaangVu\Constant;
+
+class ChatConstant
+{
+    const SUPPORTER       = "supporter";
+    const MEMBER_PASSWORD = "oFz15bjBemLvoI4Bd0Tc";
+    const Count           = 1000;
+    const Offset          = 0;
+}
