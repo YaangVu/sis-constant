2738d022b3b56f6532df9b7936d0f203c8f9f65e
dinhngochai1900 <dinhngochai1900@gmail.com>
2022-07-11 14:40:31 +0700

    add status sms teplate

---
+++ src/StatusSmsTemplateConstant.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @Author Admin
+ * @Date   Jul 11, 2022
+ */
+
+namespace YaangVu\Constant;
+
+class StatusSmsTemplateConstant
+{
+    const QUEUE     = "queue";
+    const SENT      = "sent";
+    const DELIVERED = "delivered";
+    const FAILED    = "failed";
+
+    const ALl
+        = [
+            self::QUEUE,
+            self::SENT,
+            self::DELIVERED,
+            self::FAILED,
+        ];
+}
