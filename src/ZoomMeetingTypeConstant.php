0e277d3b401e3ea9e5f4be608659fb6f5389c88f
Pham Van Tien <tienpv1@toprate.io>
2022-08-09 01:26:47 +0700

    fix: change type constant zoom-meeting

--- src/ZoomMeetingTypeConstant.php
+++ src/ZoomMeetingTypeConstant.php
@@ -6,12 +6,12 @@ namespace YaangVu\Constant;
 
 class ZoomMeetingTypeConstant
 {
-   const SCHEDULE_MEETING = "Schedule meeting";
-   const RECURRING_MEETING = "Recurring meeting with fixed time";
+    const SCHEDULE_MEETING  = "A schedule meeting";
+    const RECURRING_MEETING = "A recurring meeting with fixed time";
 
     const ALL
         = [
-            self::SCHEDULE_MEETING, 
+            self::SCHEDULE_MEETING,
             self::RECURRING_MEETING
         ];
 }
