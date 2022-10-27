caa3c26ec2a5531c855adff85c7a40f62c93af6a
haidn <haidn@toprate.io>
2022-10-27 09:46:41 +0700

    feat:add constant transcript template

---
+++ src/TranscriptTemplateConstant.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace YaangVu\Constant;
+
+class TranscriptTemplateConstant
+{
+    const DEFAULT           = 'default';
+    const HIGH_SCHOOL       = 'high school';
+    const MIDDLE_SCHOOL     = 'middle school';
+    const ELEMENTARY_SCHOOL = 'elementary school';
+
+    const ALL
+        = [
+            self::DEFAULT,
+            self::HIGH_SCHOOL,
+            self::MIDDLE_SCHOOL,
+            self::ELEMENTARY_SCHOOL
+        ];
+}
