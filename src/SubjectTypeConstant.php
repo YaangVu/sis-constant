6b33d801e115e7ce740730686d24cf496e9cc945
Pham Van Tien <tienpv1@toprate.io>
2023-03-23 15:00:21 +0700

    fix: subject type constant

--- src/SubjectTypeConstant.php
+++ src/SubjectTypeConstant.php
@@ -8,9 +8,23 @@ namespace YaangVu\Constant;
 
 class SubjectTypeConstant
 {
-    const NORMAL             = 'normal';
-    const HONORS             = 'honors';
-    const ADVANCED_PLACEMENT = 'advanced placement';
+    const   NORMAL             = 'normal';
+    const   HONORS             = 'honors';
+    const   ADVANCED_PLACEMENT = 'advanced placement';
+    const   STD_ELE            = 'STD-ELE';
+    const   STD_MID            = 'STD-MID';
+    const   HS                 = 'HS';
+    const   ELD                = 'ELD';
+    const   HONS_ELE           = 'HONS-ELE';
+    const   HONS_MID           = 'HONS-MID';
+    const   PREP_STD           = 'PREP-STD';
+    const   CP                 = 'CP';
+    const   AP                 = 'AP';
+    const   CR                 = 'CR';
+    const   HON                = 'HON';
+    const   TR                 = 'TR';
+    const   R                  = 'R';
+    const   ST                 = 'ST';
 
     const ALL
         = [
