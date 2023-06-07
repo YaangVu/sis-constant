0d27661e753572095e3e19c0db4c7337d8cb7c71
namhp <namhp>
2023-06-07 09:17:54 +0700

    add type constant term

--- src/ProgressReportTypeConstant.php
+++ src/ProgressReportTypeConstant.php
@@ -8,14 +8,18 @@ namespace YaangVu\Constant;
 
 class ProgressReportTypeConstant
 {
-    const MONTHLY_REPORT          = 'monthly_report';
-    const END_OF_SEMESTER_REPORT  = 'end_of_semester_report';
-    const MIDDLE_TERM             = 'mid_term';
+    const MONTHLY_REPORT         = 'monthly_report';
+    const END_OF_SEMESTER_REPORT = 'end_of_semester_report';
+    const MIDDLE_TERM            = 'mid_term';
+    const SEMESTER_1             = 'semester_1';
+    const SEMESTER_2             = 'semester_2';
 
     const ALL
         = [
             self::MONTHLY_REPORT,
             self::END_OF_SEMESTER_REPORT,
-            self::MIDDLE_TERM
+            self::MIDDLE_TERM,
+            self::SEMESTER_1,
+            self::SEMESTER_2
         ];
 }
---
+++ src/TermConstant.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *
+ */
+
+namespace YaangVu\Constant;
+
+class TermConstant
+{
+    const SEMESTER_1 = 1;
+    const SEMESTER_2 = 2;
+
+    const ALL
+        = [
+            self::SEMESTER_1,
+            self::SEMESTER_2
+        ];
+}
